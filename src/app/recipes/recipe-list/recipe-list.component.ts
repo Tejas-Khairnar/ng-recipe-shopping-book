25e7c46a62d97ec45e916113d91f6f3fee3e6116
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-16 14:35:11 +0530

    Adding and displaying recipes in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // stores recipe based on Recipe blueprint
+  recipes: Recipe[] = [
+    new Recipe('Test', 'Simple recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1470&q=80'),
+    new Recipe('Test', 'Simple recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1470&q=80')
+  ];
 
   constructor() { }
 
