883958fbe7b79b1799861aff01749a4c0e95f42a
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-17 12:47:24 +0530

    Added ingredient model as shared

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+// shared for both Recipes and Shopping List components
+
+import { threadId } from 'worker_threads';
+
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // shortcut for above initialization in TS, variables automatically created and initialized too
+    constructor(public name: string, public amount: number) { }
+}
