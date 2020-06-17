808fbec2645999580fc22665c67ad11d15ffc4e4
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-17 18:29:10 +0530

    Passing data with property and event binding combined

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from './../../recipe.model';
 
@@ -11,9 +11,18 @@ export class RecipeItemComponent implements OnInit {
   // get recipe from parent component i.e recipe-list component here 
   @Input() recipe: Recipe;
 
+  // define custom event emitter object for selected recipe
+  @Output() selectedRecipe = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedItem() {
+    // emit event emitter object here to its parent component i.e recipe list
+    // type void because parent already know about selected recipe item as we get recipe by property binding
+    this.selectedRecipe.emit();
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from './../recipe.model';
 
@@ -8,6 +8,9 @@ import { Recipe } from './../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // define custome event emitter object of type Recipe
+  @Output() selectedRecipeFromRecipeList = new EventEmitter<Recipe>();
+
   // stores recipe based on Recipe blueprint
   recipes: Recipe[] = [
     new Recipe('Test', 'Simple recipe', 'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1470&q=80'),
@@ -17,6 +20,13 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // get selected recipe from child component i.e recipe-item to here by event binding
+  onRecipeSelected(recipeFromRecipeItem: Recipe) {
+    console.log('recipe', recipeFromRecipeItem);
+    // emit recipe selected from recipe item component to recipes component
+    this.selectedRecipeFromRecipeList.emit(recipeFromRecipeItem);
   }
 
 }
