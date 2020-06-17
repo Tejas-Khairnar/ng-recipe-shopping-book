d9a513ce68416bee1fb236cb61716d1d720eabb6
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-17 17:35:54 +0530

    Added navigation with Event binding and *ngIF

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  // define custome event emitter object of type string
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    // emit selected feature commit from DOM selection to parent component through featureSelected object
+    this.featureSelected.emit(feature);
+  }
+}
