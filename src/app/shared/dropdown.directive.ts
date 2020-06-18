767b3ebb02f8eb66dc310f810b2ccc6fd9e7228a
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-18 14:36:34 +0530

    Close dropdown by clicking anywhere on page

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]' // directive always has attribute selector
@@ -10,7 +10,13 @@ export class DropdownDirective {
 
     // to react event happens on element on which this directive sits
     // click => HTML event of that element
-    @HostListener('click') toggleOpenClass() {
-        this.isOpen = !this.isOpen;
+    // @HostListener('click') toggleOpenClass() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    // code for closing dropdown from clicking anywhere on page
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+    constructor(private elRef: ElementRef) { }
 }
