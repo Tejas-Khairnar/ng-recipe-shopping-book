0c4da025668612fd3f8ee48e664d1837048bb906
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-22 12:43:54 +0530

    Retrieving route parameters for edit recipe - set editMode/newMode for recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  // selected recipe id
+  recipeId: number;
+  // flag to check if new recipe added OR edit existing recipe
+  editMode = false;
 
-  constructor() { }
+  // inject ActivatesRoute service here to fetch route parameter set into app-routing.module.ts
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // retrieve dynamic id from route parameters
+    this.route.params.subscribe((params: Params) => {
+      this.recipeId = +params['id']; // cast string to number using +
+      this.editMode = params['id'] != null; // if id is present then edit mode else new mode because id is undefined
+    });
   }
 
 }
