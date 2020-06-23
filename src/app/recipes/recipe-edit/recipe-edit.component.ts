33cd74f6c98a5d820818d8dab96877e0ade840d0
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-23 16:58:57 +0530

    Creating reactive form in recipe-edit inside ts file

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,15 +13,46 @@ export class RecipeEditComponent implements OnInit {
   recipeId: number;
   // flag to check if new recipe added OR edit existing recipe
   editMode = false;
+  // reactive approach to define and initializing form
+  recipeForm: FormGroup;
 
   // inject ActivatesRoute service here to fetch route parameter set into app-routing.module.ts
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     // retrieve dynamic id from route parameters
     this.route.params.subscribe((params: Params) => {
       this.recipeId = +params['id']; // cast string to number using +
       this.editMode = params['id'] != null; // if id is present then edit mode else new mode because id is undefined
+
+      // call whenever our route params changes
+      this.initForm();
+    });
+  }
+
+  // initialize reactive form here
+  private initForm() {
+    let existingRecipeName = '';
+    let existingRecipeImagePath = '';
+    let existingRecipeDescription = '';
+
+    // check if we are in edit mode
+    if (this.editMode) {
+      // select existing recipe here
+      const recipe = this.recipeService.getSingleRecipe(this.recipeId);
+      // for initial values in form if in update mode else empty for new values
+      existingRecipeName = recipe.name;
+      existingRecipeImagePath = recipe.imagePath;
+      existingRecipeDescription = recipe.description;
+    }
+
+    // get outer shell for our form
+    this.recipeForm = new FormGroup({
+      // all controls of reactive form define here as key: value pairs
+      // new FormControl(initialValue, validators/custome validators, async validators)
+      'name': new FormControl(existingRecipeName),
+      'imagePath': new FormControl(existingRecipeImagePath),
+      'description': new FormControl(existingRecipeDescription)
     });
   }
 
