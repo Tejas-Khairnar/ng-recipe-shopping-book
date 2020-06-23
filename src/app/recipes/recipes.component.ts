2fbb24a9ab9d668defa642c526aed9b56565a834
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-23 18:58:28 +0530

    Fix issue of lost recipe from recipe-list after navigate to shopping-list

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { RecipeService } from './recipe.service';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService] // 1 single instance is access here and also all its child component
+  // because of providing RecipeService here we get issue of disappearing newly added recipe 
+  // from recipe-list after navigating to shopping-list component
+  // because RecipeService instance get deleted as RecipeComponent get deleted after navigating to shopping-list component
+  // hence provide RecipeService inside AppModule
 })
 export class RecipesComponent implements OnInit {
   // no need any code here because now we get selected recipe by routing
