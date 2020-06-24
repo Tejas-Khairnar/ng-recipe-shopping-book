6b68c2b65b782c0c7e000fae650a170a453100c6
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-24 19:18:03 +0530

    Fix resolver edit recipe issue

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -4,15 +4,23 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 // this type od data need to resolve
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage-service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
     // inject here because it fires http requests
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
     // run before this route get loaded by angular automatically to remove error case of id if recipe not present
     // so always first fetch recipes from firebase before loading recipe/:id and recipe/:id/edit route
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageService.fetchRecipesFromFirebase();
+        // solve edit existing recipe get override bug here
+        const recipes = this.recipeService.getRecipes();
+
+        if (recipes.length === 0) {
+            return this.dataStorageService.fetchRecipesFromFirebase();
+        } else {
+            return recipes;
+        }
     }
 }
