b04bdb852d1b4be90513b8ce75efbf36379f8701
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-25 19:54:35 +0530

    Switch button title between login/sign up mode using property

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,4 +4,12 @@ import { Component } from '@angular/core';
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent { }
+export class AuthComponent {
+    // flag to check if user login or not
+    isLoginMode = true;
+
+    // switch button title between login or sign up mode
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
+}
