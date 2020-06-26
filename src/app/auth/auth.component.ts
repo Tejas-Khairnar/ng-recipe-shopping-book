8fd11ce6b270ead7229057dd6e316855004c024b
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-26 14:21:07 +0530

    Sending sign up request by subscribing it in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
@@ -9,6 +11,9 @@ export class AuthComponent {
     // flag to check if user login or not
     isLoginMode = true;
 
+    // inject auth service here
+    constructor(private authService: AuthService) { }
+
     // switch button title between login or sign up mode
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -16,7 +21,27 @@ export class AuthComponent {
 
     // get form data
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        if (!form.valid) {
+            return;
+        }
+
+        // extracting form inputs here
+        const email = form.value.email;
+        const password = form.value.password;
+
+        // check mode login or sign up here
+        if (this.isLoginMode) {
+            // login user here
+            // ...
+        } else {
+            // sign new user here
+            this.authService.signUpNewUser(email, password).subscribe(resData => {
+                console.log(resData);
+            }, error => {
+                console.log(error);
+            });
+        }
+
         // reset form values when we submit it
         form.reset();
     }
