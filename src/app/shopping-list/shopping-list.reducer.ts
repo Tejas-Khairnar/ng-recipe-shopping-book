c1e29b812053ef2f562c2d22a1d486164b0e99f8
Tejas-Khairnar <ktejas6892@gmail.com>
2020-06-29 13:23:37 +0530

    Added login to shopping-list reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,5 @@
+import { Action } from '@ngrx/store';
+
 import { Ingredient } from "../shared/ingredient.model";
 
 // initial state
@@ -9,6 +11,16 @@ const initialSate = {
 };
 
 // this function called by NgRx library and pass these arguments here automatically
-export function ShoppingListReducer(state = initialSate, action) {
+export function ShoppingListReducer(state = initialSate, action: Action) {
+    // multiple action tyoes we may have
+    switch (action.type) {
+        case 'ADD_INGREDIENT':
+            return {
+                // copy previous stete here
+                ...state,
+                // copy previous ingredients array here
+                ingredients: [...state.ingredients, action]
+            }
 
+    }
 }
